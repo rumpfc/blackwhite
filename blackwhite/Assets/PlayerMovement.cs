2c95494d338fe56f53a351005f4421f0dd266447
Robert <robert.tieber@gmail.com>
2015-01-24 12:07:30 +0100

    Fixed player collision

--- blackwhite/Assets/PlayerMovement.cs
+++ blackwhite/Assets/PlayerMovement.cs
@@ -4,19 +4,34 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour
 {
 	private Vector3 targetPos;
-	private float dist;
 	private bool moving;
+	private bool blockedLeft;
+	private bool blockedRight;
+
+	private float dist {
+		get { return targetPos.x - transform.position.x; }
+	}
 
 	public void collidedLeft(){
 		if(moving && dist < 0) {
 			moving = false;
+			blockedLeft = true;
 		}
 	}
 
 	public void collidedRight(){
 		if(moving && dist > 0) {
 			moving = false;
+			blockedRight = true;
 		}
+	}
+
+	public void unblockedRight() {
+		blockedRight = false;
+	}
+
+	public void unblockedLeft() {
+		blockedLeft = false;
 	}
 
 	void Update()
@@ -24,20 +39,21 @@ public class PlayerMovement : MonoBehaviour
 		if (Input.GetMouseButtonUp(0))
 		{
 			targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			moving = true;
+
+			if((dist < 0 && !blockedLeft) || (dist > 0 && !blockedRight)) {
+				moving = true;
+			}
 		}
 
 		if (moving)
 		{
-			dist = targetPos.x - transform.position.x;
-
 			if (dist > 0.1f)
 			{
-				transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime);
+				transform.Translate(new Vector3(3, 0, 0) * Time.deltaTime);
 			}
 			else if (dist < -0.1f)
 			{
-				transform.Translate(new Vector3(-1, 0, 0) * Time.deltaTime);
+				transform.Translate(new Vector3(-3, 0, 0) * Time.deltaTime);
 			}
 			else
 			{
---
+++ blackwhite/Assets/TriggerPlayerCollision.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerPlayerCollision : MonoBehaviour
+{
+
+	public PlayerMovement movement;
+	public bool right;
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (right) {
+			movement.collidedRight ();
+		} else {
+			movement.collidedLeft ();
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D other) {
+		if (right) {
+			movement.unblockedRight ();
+		} else {
+			movement.unblockedLeft ();
+		}
+
+	}
+}
