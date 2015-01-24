e68ed2cc400cd7c2d960b07ac4cd7df067a2a0f8
GideonUng <gidcheen@gmail.com>
2015-01-24 02:40:15 +0100

    Added prototype level
    Moving and switching world

---
+++ blackwhite/Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+	private Vector3 targetPos;
+	private float dist;
+	private bool moving;
+
+	void Update()
+	{
+		if (Input.GetMouseButtonUp(0))
+		{
+			targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			moving = true;
+		}
+
+		if (moving)
+		{
+			dist = targetPos.x - transform.position.x;
+
+			if (dist > 0.1f)
+			{
+				transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime);
+			}
+			else if (dist < -0.1f)
+			{
+				transform.Translate(new Vector3(-1, 0, 0) * Time.deltaTime);
+			}
+			else
+			{
+				moving = false;
+			}
+		}
+	}
+}
---
+++ blackwhite/Assets/SwitchWorld.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchWorld : MonoBehaviour
+{
+	public GameObject PlayerWhite;
+	public GameObject PlayerBlack;
+	public Camera MainCamera;
+	public Camera FlaschlightCamera;
+
+	void Update()
+	{
+		if (Input.touchCount == 1f)
+		{
+			if (Input.touches[0].phase == TouchPhase.Began)
+			{
+				
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			changeWorld();
+		}
+	}
+	void changeWorld()
+	{
+		if (MainCamera.cullingMask == 1 << 9)
+		{
+			MainCamera.cullingMask = 1 << 8;
+			PlayerBlack.SetActive(false);
+			PlayerWhite.SetActive(true);
+			MainCamera.backgroundColor = Color.black;
+		}
+		else
+		{
+			MainCamera.cullingMask = 1 << 9;
+			PlayerBlack.SetActive(true);
+			PlayerWhite.SetActive(false);
+			MainCamera.backgroundColor = Color.white;
+		}
+	}
+}
