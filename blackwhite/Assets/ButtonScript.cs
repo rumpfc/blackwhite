4debbd72cc474a22b195e43f29d6d94af0c53cae
Robert <robert.tieber@gmail.com>
2015-01-24 16:24:54 +0100

    Added tutorial level 1

---
+++ blackwhite/Assets/ButtonScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+	public Animator animator;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		animator.Play ("buttonPressed");
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		animator.Play ("buttonUnpressed");
+	}
+
+}
---
+++ blackwhite/Assets/OpenDoor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoor : MonoBehaviour {
+
+	public Animator animator;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		animator.Play ("doorOpened");
+	}
+	
+	void OnTriggerExit2D(Collider2D other) {
+		animator.Play ("doorClosed");
+	}
+}
