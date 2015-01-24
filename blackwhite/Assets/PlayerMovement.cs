f178fd2678b54caefe9185b621e781baac9e649b
Christian Rumpf <rumpf@student.tugraz.at>
2015-01-24 22:58:48 +0100

    Logs for player movement script

--- blackwhite/Assets/PlayerMovement.cs
+++ blackwhite/Assets/PlayerMovement.cs
@@ -14,7 +14,7 @@ public class PlayerMovement : MonoBehaviour
 	private bool blockedRight;
 	private bool climbableRight;
 	private bool climbableLeft;
-	private bool canJump = true;
+	public bool canJump = true;
 
 	private float dist
 	{
@@ -23,38 +23,46 @@ public class PlayerMovement : MonoBehaviour
 
 	public void canClimbRight()
 	{
+		Debug.Log ("climb to the right");
 		climbableRight = true;
 	}
 
 	public void canClimbLeft()
 	{
+		Debug.Log ("climb to the right");
 		climbableLeft = true;
 	}
 
 	public void collidedLeft()
 	{
+		Debug.Log ("Collided left");
 		if (moving && dist < 0)
 		{
+			Debug.Log ("blocked left");
 			blockedLeft = true;
 		}
 	}
 
 	public void collidedRight()
 	{
+		Debug.Log ("Collided right");
 		if (moving && dist > 0)
 		{
+			Debug.Log ("blocked right");
 			blockedRight = true;
 		}
 	}
 
 	public void unblockedRight()
 	{
+		Debug.Log ("unblocked right");
 		blockedRight = false;
 		climbableRight = false;
 	}
 
 	public void unblockedLeft()
 	{
+		Debug.Log ("unblocked left");
 		blockedLeft = false;
 		climbableLeft = false;
 	}
@@ -63,28 +71,34 @@ public class PlayerMovement : MonoBehaviour
 	{
 		if (climbableRight && moving && dist > 0)
 		{
+			Debug.Log ("climbableRight && moving and dist > 0");
 			//rigidbody2D.velocity = new Vector3(0, 4, 0);
 			if (canJump)
 			{
+				Debug.Log ("he can jump");
 				jump();
 			}
 		}
 
 		if (climbableLeft && moving && dist < 0)
 		{
+			Debug.Log ("climbableLeft && moving && dist < 0");
 			//rigidbody2D.velocity = new Vector3(0, 4, 0);
 			if (canJump)
 			{
+				Debug.Log ("he can jump");
 				jump();
 			}
 		}
 
 		if (Input.GetMouseButtonUp(0))
 		{
+			Debug.Log ("Mouse Button up");
 			targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
+			Debug.Log ("targetPos = " + targetPos);
 			if ((dist < 0 && !blockedLeft) || (dist > 0 && !blockedRight))
 			{
+				Debug.Log ("(dist < 0 && !blockedLeft) || (dist > 0 && !blockedRight)");
 				moving = true;
 				GetComponent<Animator>().SetTrigger("Start");
 			}
@@ -92,36 +106,42 @@ public class PlayerMovement : MonoBehaviour
 
 		if (moving)
 		{
+			Debug.Log ("I like to move it");
 			if (dist > 0.1f)
 			{
+				Debug.Log ("dist > 0.1");
 				if (!blockedRight)
 				{
+					Debug.Log ("not blocked right");
 					transform.Translate(new Vector3(Speed, 0, 0) * Time.deltaTime);
 					PlayerWhite.transform.localScale = new Vector3(1, 1, 1);
 					PlayerBlack.transform.localScale = new Vector3(1, 1, 1);
 				}
 				else
 				{
-					Debug.Log("blockedRight");
+					Debug.Log("blocked right");
 					StopWalking();
 				}
 			}
 			else if (dist < -0.1f)
 			{
+				Debug.Log("dist < -0.1");
 				if (!blockedLeft)
 				{
+					Debug.Log ("not blocked left");
 					transform.Translate(new Vector3(-Speed, 0, 0) * Time.deltaTime);
 					PlayerWhite.transform.localScale = new Vector3(-1, 1, 1);
 					PlayerBlack.transform.localScale = new Vector3(-1, 1, 1);
 				}
 				else
 				{
-					Debug.Log("blockedRight");
+					Debug.Log("blocked left");
 					StopWalking();
 				}
 			}
 			else
 			{
+				Debug.Log ("STOOOOP");
 				StopWalking();
 			}
 		}
@@ -129,26 +149,32 @@ public class PlayerMovement : MonoBehaviour
 
 	void jump()
 	{
+		Debug.Log ("I'm jumping");
 		canJump = false;
 		GetComponent<Rigidbody2D>().isKinematic = true;
 		if (PlayerWhite.transform.localScale.x == 1)
 		{
+			Debug.Log ("jumping to the right");
 			GetComponent<Animator>().SetTrigger("JumpRight");
 		}
 		else
 		{
+			Debug.Log ("jumping to the left");
 			GetComponent<Animator>().SetTrigger("JumpLeft");
 		}
 	}
 
 	void stopJump()
 	{
+		Debug.Log ("finished jumping");
 		if (PlayerWhite.transform.localScale.x == 1)
 		{
+			Debug.Log ("I jumped to the right");
 			transform.Translate(new Vector3(1.125f, 1.001f, 0));
 		}
 		else
 		{
+			Debug.Log ("I jumped to the left");
 			transform.Translate(new Vector3(-1.125f, 1.001f, 0));
 		}
 		GetComponent<Rigidbody2D>().isKinematic = false;
@@ -158,7 +184,7 @@ public class PlayerMovement : MonoBehaviour
 
 	public void StopWalking()
 	{
-		Debug.Log("StopWalking() called");
+		Debug.Log("I stop walking");
 		moving = false;
 		GetComponent<Animator>().SetTrigger("Stop");
 	}
