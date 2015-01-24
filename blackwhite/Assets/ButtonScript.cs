9ddf04587de93102ab81d8457887fd126b106d6a
GideonUng <gidcheen@gmail.com>
2015-01-24 19:45:02 +0100

    increased player collider yize

--- blackwhite/Assets/ButtonScript.cs
+++ blackwhite/Assets/ButtonScript.cs
@@ -1,16 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ButtonScript : MonoBehaviour {
+public class ButtonScript : MonoBehaviour
+{
 
 	public Animator animator;
 
-	void OnTriggerEnter2D(Collider2D other) {
-		animator.Play ("buttonPressed");
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		animator.Play("buttonPressed");
 	}
 
-	void OnTriggerExit2D(Collider2D other) {
-		animator.Play ("buttonUnpressed");
+	void OnTriggerExit2D(Collider2D other)
+	{
+		animator.Play("buttonUnpressed");
 	}
 
 }
