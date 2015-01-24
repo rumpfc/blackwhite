3ad662ea1ddd0cef180fbedb36b980d1e2ca2b0c
Christian Rumpf <rumpf@student.tugraz.at>
2015-01-24 11:44:25 +0100

    Smooth Player Camera Movements

---
+++ blackwhite/Assets/MainCameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCameraMovement : MonoBehaviour {
+
+	public GameObject Player;
+	private Vector3 velocity = Vector3.zero;
+	private float smoothTime = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newCamPos = Player.transform.position;
+		newCamPos.z = transform.position.z;
+		transform.position =  Vector3.SmoothDamp(transform.position, newCamPos, ref velocity, smoothTime);
+	}
+}
