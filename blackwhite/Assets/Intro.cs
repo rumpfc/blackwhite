ecaeaaefb914202314713711684474aa33cfa6f5
Christian Rumpf <rumpf@student.tugraz.at>
2015-01-25 13:54:48 +0100

    Intro update

--- blackwhite/Assets/Intro.cs
+++ blackwhite/Assets/Intro.cs
@@ -19,15 +19,24 @@ public class Intro : MonoBehaviour
 	{
 		foreach(GameObject t in Texts)
 		{
+			Animator textAnim = t.GetComponent<Animator>();
+			if(textAnim != null)
+				textAnim.SetTrigger("blend");
+
 			t.SetActive(true);
 
-			yield return new WaitForSeconds(4);
+			yield return new WaitForSeconds(3);
+			if(textAnim != null)
+				textAnim.SetTrigger("blend");
 
+			yield return new WaitForSeconds(1);
 			t.SetActive(false);
 		}
 
 		PlayerAnim.SetTrigger("JumpRight");
 
+		Me.GetComponent<Animator> ().SetTrigger ("blend");
+		Myself.GetComponent<Animator> ().SetTrigger ("blend");
 		Me.SetActive(true);
 		Myself.SetActive(true);
 
