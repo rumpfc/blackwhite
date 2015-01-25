69923cdbcee8f1ba5113d3ce350598f516e838f2
GideonUng <gidcheen@gmail.com>
2015-01-25 11:22:02 +0100

    added to paralax

--- blackwhite/Assets/Paralax.cs
+++ blackwhite/Assets/Paralax.cs
@@ -6,8 +6,14 @@ public class Paralax : MonoBehaviour
 	public float Multiplicator;
 	public GameObject MainCamera;
 
+	private Vector3 relPos;
+	void Start()
+	{
+		relPos = transform.position - MainCamera.transform.position;
+	}
+
 	void Update()
 	{
-		transform.position = new Vector3( MainCamera.transform.position.x, MainCamera.transform.position.y, 3) * Multiplicator;
+		transform.position = new Vector3(MainCamera.transform.position.x + relPos.x, MainCamera.transform.position.y + relPos.y, 3) * Multiplicator;
 	}
 }
