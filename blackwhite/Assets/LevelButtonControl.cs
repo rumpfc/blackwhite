ebcb127d55bfc65ee273b6fad6c772e872653967
Christian Rumpf <rumpf@student.tugraz.at>
2015-01-25 15:01:53 +0100

    added buttons (not working)

---
+++ blackwhite/Assets/LevelButtonControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelButtonControl : MonoBehaviour {
+
+	public void returnToMainMenu()
+	{
+		Application.LoadLevel ("MainMenu");
+	}
+
+	public void reloadLevel()
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
