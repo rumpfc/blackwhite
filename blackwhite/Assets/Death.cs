96deb6a708ab98a664ab40eb166cf5484759daa9
GideonUng <gidcheen@gmail.com>
2015-01-25 14:14:11 +0100

    added death

---
+++ blackwhite/Assets/Death.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Death : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
