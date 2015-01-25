bee73f6f74dfaa28c461f6213a16278aa2e932f5
GideonUng <gidcheen@gmail.com>
2015-01-25 10:35:30 +0100

    added change level script

---
+++ blackwhite/Assets/NextLevel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevel : MonoBehaviour
+{
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.collider2D.tag == "Player")
+		{
+			Application.LoadLevel(Application.loadedLevel + 1);
+		}
+	}
+}
